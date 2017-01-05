e11645d77eb066dbd710e1c0a9fa71887f2ce75a
Harry Radford <harry.radford@live.co.uk>
2017-01-05 23:32:51 +0000

    Update middleware for localhost

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var app = express();
 const PORT = process.env.PORT || 3000; //Accessing environment variable with Node. Heroku passes us the port.
 
 app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
