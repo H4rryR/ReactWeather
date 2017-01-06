73b63ae4f88d68fdac4b3408a7198045f72e47af
Harry Radford <harry.radford@live.co.uk>
2017-01-06 23:48:59 +0000

    Style pages

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are some example locations to try:</p>
+      <ul>
+        <li>
+          <Link to="/?location=Namibia">Namibia</Link>
+        </li>
+        <li>
+          <Link to="/?location=London">London</Link>
+        </li>
+        <li>
+          <Link to="/?location=California">California</Link>
+        </li>
+      </ul>
     </div>
   );
 }
