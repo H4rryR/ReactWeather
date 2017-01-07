414b2beb6dfa012350b204528637bc2ed072c0fe
Harry Radford <harry.radford@live.co.uk>
2017-01-07 00:13:30 +0000

    Style about page & add content

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,23 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is an application that retreives the weather from a secific
+        location using the Open Weather Map API.
+      </p>
+      <p>
+        I built this app using The Complete React Web App Course at Udemy
+        to learn how to make web applications with React.
+      </p>
+      <ul>
+        <li>
+          <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/overview">The Complete React Web App Course</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a>
+        </li>
+      </ul>
     </div>
   );
 }
