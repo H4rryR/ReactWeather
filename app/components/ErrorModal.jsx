de44eb51acfcd747555c9a137810f56dbbec4892
Harry Radford <harry.radford@live.co.uk>
2017-01-08 19:35:44 +0000

    Fix modal bug

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
 
@@ -13,21 +15,34 @@ var ErrorModal = React.createClass({
     message: React.PropTypes.string.isRequired
   },
 
+  //Rendering in here instead as Foundation renders to the DOM.
+  //React does not like this.
   componentDidMount: function () {
-    //Modals hidden by default, this opens it when it's rendered.
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  },
-
-  render: function () {
     var {title, message} = this.props;
-    return (
+
+    var modalMarkup = (
       <div id="error-modal" className="reveal text-center tiny" data-reveal="">
         <h3>{title}</h3>
         <p>{message}</p>
         <div>
           <button className="button" data-close="">Okay</button>
         </div>
+      </div>
+    );
+
+    //Returns string version of jsx code.
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    //Modals hidden by default, this opens it when it's rendered.
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+
+  render: function () {
+
+    return (
+      <div>
       </div>
     );
   }
